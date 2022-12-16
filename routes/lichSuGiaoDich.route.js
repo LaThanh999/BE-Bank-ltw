8915302680cbb2e4935586af4c6003cd4d507d5b
Nhat Tan <staan0906@gmail.com>
2022-12-16 22:56:43 +0700

    add routes

---
+++ routes/lichSuGiaoDich.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+const { getAll, getById, insert, edit, remove } = require('../controllers/lichSuGiaoDich.controllers');
+const mdwValidate = require('../middlewares/validate.mdw');
+const schema = require('../schemas/lichSuGiaoDich.json');
+
+router.get('/', getAll);
+router.get('/:id', getById);
+router.post('/', mdwValidate(schema), insert);
+router.delete('/:id', remove);
+router.put('/:id', mdwValidate(schema), edit);
+
+module.exports = router;
