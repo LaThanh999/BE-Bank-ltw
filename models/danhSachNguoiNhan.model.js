067af598e0cc78a9b9d896afd6c55570a3f80a88
LeQuy <lehquy99@gmail.com>
2022-12-28 15:00:22 +0700

    fix bug

--- models/danhSachNguoiNhan.model.js
+++ models/danhSachNguoiNhan.model.js
@@ -9,8 +9,9 @@ module.exports = {
 
     var result = await db('danhSachNguoiNhan as DSNN ')
       .join('taiKhoan as TKNhan', 'DSNN.maTaiKhoanNguoiNhan', '=', 'TKNhan.maTaiKhoan')
+      .join('nganHangDoiTac as NH', 'DSNN.idNganHang', '=', 'NH.id')
       .where({ maTaiKhoanNguoiChuyen: value })
-      .select('DSNN.*', 'TKNhan.hoTen as hoTenNguoiNhan')
+      .select('DSNN.*', 'TKNhan.hoTen as hoTenNguoiNhan','NH.tenNganHang')
       .orderBy('id', 'asc');
     if (!result) {
       return null;
