0b87a0aae245c902e7f22c04d63ff5dd6b965bd4
ThanhLC2 <thanhlc2@vng.com.vn>
2023-01-03 09:59:20 +0700

    check deploy

--- models/lichSuGiaoDich.model.js
+++ models/lichSuGiaoDich.model.js
@@ -1,39 +1,39 @@
 const db = require('../utils/db');
 
 module.exports = {
-  getAll() {
-    return db('lichSuGiaoDich');
-  },
-  async getFindById(id) {
-    const result = await db('lichSuGiaoDich').where({ id: id });
-    if (!result) {
-      return null;
-    }
-    return result[0];
-  },
-  async getByAccountNumber(value) {
-    var result = await db('lichSuGiaoDich as LSGD')
-      .join('taiKhoan as TKNhan', 'LSGD.taiKhoanNguoiNhan', '=', 'TKNhan.maTaiKhoan')
-      .join('taiKhoan as TKGui', 'LSGD.taiKhoanNguoiGui', '=', 'TKGui.maTaiKhoan')
-      .where({ taiKhoanNguoiGui: value })
-      .orWhere({ taiKhoanNguoiNhan: value })
-      .select('LSGD.*', 'TKNhan.hoTen as hoTenNguoiNhan', 'TKGui.hoTen as hoTenNguoiGui')
-      .orderBy('id', 'asc');
+  // getAll() {
+  //   return db('lichSuGiaoDich');
+  // },
+  // async getFindById(id) {
+  //   const result = await db('lichSuGiaoDich').where({ id: id });
+  //   if (!result) {
+  //     return null;
+  //   }
+  //   return result[0];
+  // },
+  // async getByAccountNumber(value) {
+  //   var result = await db('lichSuGiaoDich as LSGD')
+  //     .join('taiKhoan as TKNhan', 'LSGD.taiKhoanNguoiNhan', '=', 'TKNhan.maTaiKhoan')
+  //     .join('taiKhoan as TKGui', 'LSGD.taiKhoanNguoiGui', '=', 'TKGui.maTaiKhoan')
+  //     .where({ taiKhoanNguoiGui: value })
+  //     .orWhere({ taiKhoanNguoiNhan: value })
+  //     .select('LSGD.*', 'TKNhan.hoTen as hoTenNguoiNhan', 'TKGui.hoTen as hoTenNguoiGui')
+  //     .orderBy('id', 'asc');
 
-    if (!result) {
-      return null;
-    }
-    return result;
-  },
+  //   if (!result) {
+  //     return null;
+  //   }
+  //   return result;
+  // },
 
   add(lichSuGiaoDich) {
     return db('lichSuGiaoDich').insert(lichSuGiaoDich);
   },
 
-  remove(id) {
-    return db('lichSuGiaoDich').where('id', id).del();
-  },
-  update(id, lichSuGiaoDich) {
-    return db('lichSuGiaoDich').where('id', id).update(lichSuGiaoDich);
-  },
+  // remove(id) {
+  //   return db('lichSuGiaoDich').where('id', id).del();
+  // },
+  // update(id, lichSuGiaoDich) {
+  //   return db('lichSuGiaoDich').where('id', id).update(lichSuGiaoDich);
+  // },
 };
